937f7c41263cb1a5214220061a705085542b2b1f
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-17 20:48:11 -0600

    add code for left and right click

--- mouse.py
+++ mouse.py
@@ -2,6 +2,8 @@ import cv2
 import mediapipe
 import pyautogui
 
+#473 es el punto de iris
+
 cam = cv2.VideoCapture(0)
 faceMesh=mediapipe.solutions.face_mesh.FaceMesh(static_image_mode=False, refine_landmarks=True)
 screenW, screenH=pyautogui.size()
@@ -15,14 +17,42 @@ while (cam.isOpened()):
     frameH,frameW,framez=frame.shape
     if (landMarkPoints):
         landMarks=landMarkPoints[0].landmark
-        for id, landmark in enumerate(landMarks[474:478]):
+        for id, landmark in enumerate(landMarks[473:478]):
             #x=landMarks[474].x
-            #y=landMarks[474].y
-            x=int(landMarks[374].x * frameW)
-            y=int(landMarks[374].y * frameH)
-            cv2.circle(frame,(x,y),3,(0,0,255))
-            if (cv2.waitKey(1)==ord('w')):
-                pyautogui.click(button="right")
+            #y=landMarks[474].[473:478]y
+            irisx=int(landMarks[473].x*frameW)
+            irisy=int(landMarks[473].y*frameH)
+            cv2.circle(frame, (irisx,irisy),3,(0,0,255))
+            if id == 1:
+               screenX=screenW*landMarks[473].x
+               screenY=screenH*landMarks[473].y
+               pyautogui.moveTo(screenX,screenY)
+
+        #se define un arreglo con las coordenadas del ojo derecho
+        right=[landMarks[374], landMarks[386]]
+        for landmark in right:
+            x=int(landmark.x*frameW)
+            y=int(landmark.y*frameH)
+            cv2.circle(frame, (x,y),3,(0,0,255))
+        if (right[0].y-right[1].y)< 0.002:
+            pyautogui.click(button="right")
+            pyautogui.sleep(1)
+
+        #se define un arreglo con las coordenadas del ojo izquierdo
+        left=[landMarks[145], landMarks[159]]
+        for landmark in left:
+            x=int(landmark.x*frameW)
+            y=int(landmark.y*frameH)
+            cv2.circle(frame, (x,y),3,(0,255,255))
+        if (left[0].y-left[1].y) < 0.002:
+            pyautogui.click()
+            pyautogui.sleep(1)
+
+            #x=int(landMarks[6].x * frameW)
+            #y=int(landMarks[6].y * frameH)
+            #cv2.circle(frame,(x,y),3,(0,0,255))
+            #if (cv2.waitKey(1)==ord('w')):
+                #pyautogui.click(button="right")
            # if id==1:
                # screenX=(screenW/frameW)*x
                # screenY=(screenH/frameH)*y
